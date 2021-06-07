aebb4a05f37842c1bbac27f425288c470b69ee6b
gitnhp <gitnhp@163.com>
2021-06-07 21:32:11 +0800

    sixth commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -6,7 +6,7 @@ public class GitTest {
         System.out.println("hello,world----1!");
         System.out.println("hello,world----2!");
         System.out.println("hello,world----3!");
-        System.out.println("hello,world----4!");
+        System.out.println("hello,world----4----hotfix!");
 
 
     }
